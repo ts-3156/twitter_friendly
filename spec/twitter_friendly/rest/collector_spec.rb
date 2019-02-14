cb430c8f814e51f7e325fe3a15990a5b9e3bc4f2
ts-3156 <ts_3156@yahoo.co.jp>
2019-02-14 22:57:42 +0900

    Add test cases to collector_spec.rb

--- spec/twitter_friendly/rest/collector_spec.rb
+++ spec/twitter_friendly/rest/collector_spec.rb
@@ -3,8 +3,11 @@ module TwitterFriendly
     ::RSpec.describe Collector do
       let(:dummy_class) do
         Class.new do
+          def hello(options = {})
+            "hello #{options}"
+          end
+
           include Collector
-          include Timelines
         end
       end
 
@@ -13,6 +16,29 @@ module TwitterFriendly
       end
       let(:internal_client) { client.instance_variable_get(:@twitter) }
 
+      describe '#fetch_tweets_with_max_id' do
+        let(:method_name) { :hello }
+        it do
+          expect(client).to receive(:hello).with(any_args)
+          client.fetch_tweets_with_max_id(method_name, 100)
+        end
+      end
+
+      describe '#fetch_resources_with_cursor' do
+        let(:method_name) { :hello }
+        it do
+          expect(client).to receive(:hello).with(any_args)
+          client.fetch_resources_with_cursor(method_name)
+        end
+      end
+
+      describe '#collect_with_max_id' do
+
+      end
+
+      describe '#collect_with_cursor' do
+
+      end
     end
   end
 end
